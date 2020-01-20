79d68319a5328cccd6c9e55c625863332e796516
Boi Moriba <boi.moriba@gmail.com>
2020-01-20 22:54:28 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -17,7 +17,7 @@ function destructivelyRemoveFirstKitten(name){
 
 
 function appendKitten(name) {
-return concat(name)
+new Array = ['Milo', 'Otis', 'Garfield'].concat(name)
 }
 
 function prependKitten(name){
